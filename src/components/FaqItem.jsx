652eb9b73112a53bee017ddd3807012bcdeb0b72
NandiniBure <nandinibure@gmail.com>
2025-02-02 16:18:42 +0530

    Xora website clone

--- src/components/FaqItem.jsx
+++ src/components/FaqItem.jsx
@@ -1,6 +1,6 @@
 import clsx from "clsx";
 import { useState } from "react";
-import SlideDown from "react-slidedown";
+import { Collapse } from 'react-collapse';
 import "react-slidedown/lib/slidedown.css"
 
 
@@ -25,10 +25,10 @@ const FaqItem = ({item,index}) => {
             <div className="g4 size-11/12 rounded-full shadow-300"/>
         </div>
      </div>
-     <SlideDown>
+     <Collapse>
         {activeId===item.id &&
         <div className="body-3 px-7 py-3.5">{item.answer}</div>}
-     </SlideDown>
+     </Collapse>
      <div className={clsx("g5 -bottom-7 -top-7 left-0 right-0 -z-1 rounded-3xl opacity-0 transition-opacity duration-500 absolute",
      active && "opacity-100" )}>
         <div className="g4 absolute inset-0.5 -z-1 rounded-3xl"/>
